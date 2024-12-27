1896e96a57b24a094285e075109d890b480b5f44
sHRLo <62970136+sHRLo@users.noreply.github.com>
2024-12-27 09:30:14 +0000

    Modify

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
-/* eslint-disable no-unused-vars */
-import React from 'react'
-import ReactDom from 'react-dom';
-
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App';
+import App from './App.jsx'
 
-ReactDom.render(<App/>, document.getElementById('root'));
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
