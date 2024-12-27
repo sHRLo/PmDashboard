082213f5c82badae57b2f6cb2135701ee110f7c2
sHRLo <62970136+sHRLo@users.noreply.github.com>
2024-12-27 08:41:13 +0000

    modify

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+import './App.css'
+
+const App = () => {
+  return (
+    <h1 className='underline text-3xl'>App</h1>
+  )
+}
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import ReactDom from 'react-dom';
 
-import App from './App'
+import './index.css'
+import App from './App';
 
 ReactDom.render(<App/>, document.getElementById('root'));
